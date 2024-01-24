b9033ae53b85f107b00a208917f541a967bf33a5
YaoTeacher <2354413037@qq.com>
2024-01-24 16:58:37 +0800

    update pathfind

--- Assets/Scripts/PathFind/AreaData.cs
+++ Assets/Scripts/PathFind/AreaData.cs
@@ -8,8 +8,72 @@ public class AreaData:BaseData
     public bool IsIndoor;
     public Vector3Int firstBorderPoint;
     public Vector3Int lastBorderPoint;
-    public PathData[,,] points;
 
+    public Dictionary<Vector3Int,PathData> GroundData = new Dictionary<Vector3Int,PathData>();
+
+    public List<Vector3Int> GetNeibor(PathData path)
+    {
+        List<Vector3Int> neibor = new List<Vector3Int>();
+
+        for (int z = -1; z <= 1;z++)
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 1 && z == 1)
+                    {
+                        if (GroundData[path.WorldIndex + new Vector3Int(1, y, 0)] != null && GroundData[path.WorldIndex + new Vector3Int(0, y, 1)] != null)
+                        {
+                            neibor.Add(path.WorldIndex + new Vector3Int(x, y, z));
+                            continue;
+                        }
+                        else
+                            continue;
+                    }
+                    if (x == -1 && z == 1)
+                    {
+                        if (GroundData[path.WorldIndex + new Vector3Int(-1, y, 0)] != null && GroundData[path.WorldIndex + new Vector3Int(0, y, 1)] != null)
+                        {
+                            neibor.Add(path.WorldIndex + new Vector3Int(x, y, z));
+                            continue;
+                        }
+                        else
+                            continue;
+                    }
+                    if (x == 1 && z == -1)
+                    {
+                        if (GroundData[path.WorldIndex + new Vector3Int(1, y, 0)] != null && GroundData[path.WorldIndex + new Vector3Int(0, y, -1)] != null)
+                        {
+                            neibor.Add(path.WorldIndex + new Vector3Int(x, y, z));
+                            continue;
+                        }
+                        else
+                            continue;
+                    }
+                    if (x == -1 && z == -1)
+                    {
+                        if (GroundData[path.WorldIndex + new Vector3Int(11, y, 0)] != null && GroundData[path.WorldIndex + new Vector3Int(0, y, -1)] != null)
+                        {
+                            neibor.Add(path.WorldIndex + new Vector3Int(x, y, z));
+                            continue;
+                        }
+                        else
+                            continue;
+                    }
+                    if (GroundData[path.WorldIndex + new Vector3Int(x, y, z)] != null)
+                    {
+
+                        neibor.Add(path.WorldIndex + new Vector3Int(x, y, z));
+                    }
+                    else
+                        continue;
+                }
+            }
+        }
+
+        return neibor;
+    }
 }
 
 public class PathData: BaseData 
@@ -17,6 +81,7 @@ public class PathData: BaseData
     public int type;
     public bool IsGround;
     public bool IsAccessable;
+    public Vector3Int WorldIndex;
 }
 
 public class FlowMap : BaseData 
